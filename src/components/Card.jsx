7125b1cf5ce5b1a1020c2877d5915e52caf51f9e
tiagooo0 <thurstaltamirano@escuelasproa.edu.ar>
2024-06-24 10:21:14 -0300

    cambio un poco el front

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,6 +17,8 @@ function Card() {
           <div className='group-hover:stroke-white flex flex-col items-center justify-center' key={product.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', width: '300px' }}>
             <img src={product.image}  className="object-contain h-48 w-full" />
             <h2 className='text-white  text-center'>{product.title}</h2>
+            <strong><p className='text-white  text-center'>${product.price}</p></strong> 
+
           </div>
         ))}
       </div>
